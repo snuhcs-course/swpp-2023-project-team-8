d14e2ca591aded405a0f1eca0d82a771325e3ce8
leeeryboy <indiv0227@snu.ac.kr>
2023-11-16 17:10:11 +0900

    test: 친구 기능

--- backend/spec/models/user/friendable_spec.rb
+++ backend/spec/models/user/friendable_spec.rb
@@ -5,6 +5,7 @@ require "rails_helper"
 RSpec.describe User::Friendable do
   let!(:user) { User.create!(email: "valid@snu.ac.kr", name: "test", password: "1234") }
   let!(:user2) { User.create!(email: "valid2@snu.ac.kr", name: "test2", password: "1234") }
+  let!(:user3) { User.create!(email: "valid3@snu.ac.kr", name: "test3", password: "1234") }
 
   describe "#befriend" do
     context "이미 친구일 경우" do
@@ -14,6 +15,17 @@ RSpec.describe User::Friendable do
 
         user2.befriend(user)
         expect(Friendship.count).to eq(1)
+      end
+    end
+
+    it "스스로에게 친구 요청을 보낼 수 없다" do
+      expect(user.befriend(user)).to be_nil
+    end
+
+    context "이미 친구 요청을 보냈으면" do
+      it "다시 보낼 수 없다" do
+        user.befriend(user2)
+        expect(user.befriend(user2)).to be_nil
       end
     end
   end
@@ -32,6 +44,40 @@ RSpec.describe User::Friendable do
         user.confirm_friendship(user2)
 
         expect(user.requested_friendships.first.confirmed?).to be_truthy
+      end
+    end
+  end
+
+  describe "#unfriend" do
+    it "친구 삭제를 할 수 있다" do
+      user.befriend(user2)
+      user2.confirm_friendship(user)
+      expect(user.confirmed_friends).to include(user2)
+      expect(user2.inverse_confirmed_friends).to include(user)
+
+      user.unfriend(user2)
+      expect(user.confirmed_friends).not_to include(user2)
+      expect(user2.inverse_confirmed_friends).not_to include(user)
+    end
+  end
+
+  describe "#all_friends" do
+    it "모든 친구를 확인할 수 있다" do
+      user.befriend(user2)
+      user2.confirm_friendship(user)
+
+      user3.befriend(user)
+      user.confirm_friendship(user3)
+
+      expect(user.all_friends).to include(user2, user3)
+    end
+  end
+
+  describe "#pending_friendships" do
+    context "친구 요청이 왔을때" do
+      it "확인할 수 있다" do
+        user2.befriend(user)
+        expect(user.pending_friendships).to include(user2.friendships.first)
       end
     end
   end
