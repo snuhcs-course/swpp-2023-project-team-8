157a60fc7afc967b5a37f8d906bb872992a0df9b
Miller <miller@daangn.com>
2023-11-18 18:31:24 +0900

    REST API 추가하기

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -11,5 +11,9 @@ Rails.application.routes.draw do
     post 'confirm', on: :member
   end
 
+  resources :places do
+    get :recommend, on: :collection
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
