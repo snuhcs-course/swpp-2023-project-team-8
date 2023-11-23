5da7121e0db9a098893ea6d3fb508b79b6e9f464
Miller <miller@daangn.com>
2023-11-23 18:57:49 +0900

    Add `.last_check_in_by_user`

--- backend/app/models/check_in.rb
+++ backend/app/models/check_in.rb
@@ -1,7 +1,9 @@
 class CheckIn < ApplicationRecord
   belongs_to :user
 
-  scope :order_by_distance, -> (latitude, longitude) do
+  scope :order_by_distance, -> (latitude, longitude) {
     order(Arel.sql("ABS(latitude - #{latitude}) + ABS(longitude - #{longitude})"))
-  end
+  }
+
+  scope :last_check_in_by_user, -> { select('DISTINCT ON (user_id) *').order('user_id, created_at DESC') }
 end
--- backend/spec/models/check_in_spec.rb
+++ backend/spec/models/check_in_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe CheckIn, type: :model do
       expect(described_class.order_by_distance(126.0, 37.0).first).to eq(check_in1)
     end
   end
+
+  describe ".last_check_in_by_user" do
+    let!(:user1) { User.create!(email: "user1@snu.ac.kr", name: "user1", password: "1234") }
+    let!(:user2) { User.create!(email: "user2@snu.ac.kr", name: "user2", password: "1234") }
+    let!(:check_in1) { CheckIn.create!(user: user1, latitude: 126.1, longitude: 37.1, created_at: 1.day.ago) }
+    let!(:check_in2) { CheckIn.create!(user: user1, latitude: 120.1, longitude: 37.1, created_at: 2.days.ago) }
+    let!(:check_in3) { CheckIn.create!(user: user2, latitude: 120.1, longitude: 37.1, created_at: 1.day.ago) }
+    let!(:check_in4) { CheckIn.create!(user: user2, latitude: 126.1, longitude: 37.1, created_at: 2.days.ago) }
+
+    it "returns the last check-in for each user" do
+      expect(described_class.last_check_in_by_user).to match_array([check_in1, check_in3])
+    end
+  end
 end
