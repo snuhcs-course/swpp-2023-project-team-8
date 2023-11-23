40ec1ff80f44c118a2711ec21061c8bee2d305c2
Miller <miller@daangn.com>
2023-11-23 18:51:23 +0900

    Add `order_by_distance`

--- backend/app/models/check_in.rb
+++ backend/app/models/check_in.rb
@@ -1,3 +1,7 @@
 class CheckIn < ApplicationRecord
   belongs_to :user
+
+  scope :order_by_distance, -> (latitude, longitude) do
+    order(Arel.sql("ABS(latitude - #{latitude}) + ABS(longitude - #{longitude})"))
+  end
 end
--- backend/spec/models/check_in_spec.rb
+++ backend/spec/models/check_in_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe CheckIn, type: :model do
+  describe ".order_by_distance" do
+    let!(:user) { User.create!(email: "valid@snu.ac.kr", name: "test", password: "1234") }
+    let!(:check_in1) { CheckIn.create!(user: user, latitude: 126.1, longitude: 37.1) }
+    let!(:check_in2) { CheckIn.create!(user: user, latitude: 120.1, longitude: 37.1) }
+
+    it "returns the nearest check-in based on latitude and longitude" do
+      expect(described_class.order_by_distance(126.0, 37.0).first).to eq(check_in1)
+    end
+  end
 end
