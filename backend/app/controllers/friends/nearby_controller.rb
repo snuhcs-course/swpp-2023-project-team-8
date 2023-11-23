743c86af97ca114e821ce9061b42ad3a4bbafdad
Miller <miller@daangn.com>
2023-11-23 20:13:16 +0900

    TC 추가

--- backend/app/controllers/friends/nearby_controller.rb
+++ backend/app/controllers/friends/nearby_controller.rb
@@ -2,9 +2,7 @@
 
 class Friends::NearbyController < ApplicationController
   def index
-    render json: {
-      friends: current_user.nearby_friends
-    }, each_serializer: User::WithLocationSerializer
+    render json: current_user.nearby_friends, each_serializer: User::WithLocationSerializer
   rescue ActiveRecord::RecordNotFound => e
     render json: {error: "#{e.model} not found"}, status: :not_found
   end
--- backend/spec/controllers/friends/nearby_controller_spec.rb
+++ backend/spec/controllers/friends/nearby_controller_spec.rb
@@ -12,7 +12,33 @@ RSpec.describe Friends::NearbyController, type: :request do
         get "/friends/nearby", headers: auth_header(user)
 
         expect(response).to have_http_status(:ok)
-        expect(parsed_body["friends"]).to eq []
+        expect(parsed_body).to eq []
+      end
+    end
+
+    context "when there are nearby friends" do
+      let!(:friend) { User.create!(email: "friend@snu.ac.kr", name: "friend", password: "1234") }
+      let!(:friend_check_in) { CheckIn.create!(user: friend, latitude: 37.449, longitude: 126.953) }
+      let!(:friendship) { Friendship.create!(user: user, friend: friend, confirmed: true) }
+
+      it "returns the nearby friends" do
+        get "/friends/nearby", headers: auth_header(user)
+
+        expect(response).to have_http_status(:ok)
+        expect(parsed_body[0]["latitude"]).to eq(37.449)
+        expect(parsed_body[0]["longitude"]).to eq(126.953)
+      end
+    end
+
+    context "when there are user but they are not friend" do
+      let!(:far_friend) { User.create!(email: "far_friend@snu.ac.kr", name: "far_friend", password: "1234") }
+      let!(:far_friend_check_in) { CheckIn.create!(user: far_friend, latitude: 38.449, longitude: 127.953) }
+
+      it "returns an empty array" do
+        get "/friends/nearby", headers: auth_header(user)
+
+        expect(response).to have_http_status(:ok)
+        expect(parsed_body).to eq []
       end
     end
   end
