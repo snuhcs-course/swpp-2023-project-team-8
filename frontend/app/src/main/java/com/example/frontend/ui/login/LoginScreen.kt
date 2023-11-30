a958d884afc0235ecaa9ca4a3b1f9a0fc2500e95
Miller <miller@daangn.com>
2023-11-30 22:32:32 +0900

    Change main screen

--- frontend/app/src/main/java/com/example/frontend/ui/login/LoginScreen.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/login/LoginScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,7 +42,9 @@ fun LoginScreen() {
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 100.dp)
     ) {
         Text(
             text = currentComponent.value.title,
@@ -73,10 +76,11 @@ fun LoginScreen() {
             )
         }
 
-        if (true) {
-            Text(text = "호에엥")
-        } else {
-            Text(text = "ㅎ이잉잉")
+        Spacer(modifier = Modifier.height(40.dp))
+
+        when (currentComponent.value) {
+            LoginComponent.LOGIN -> LoginUI()
+            LoginComponent.REGISTER -> RegisterUI(onRegisterClick)
         }
     }
 }
