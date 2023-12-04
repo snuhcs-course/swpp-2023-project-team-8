d11c1192722a3fadd4cbf2bab18d06a0fee15623
heka1024 <heka1024@gmail.com>
2023-12-04 21:50:17 +0900

    Add keyboardOptions

--- frontend/app/src/main/java/com/example/frontend/ui/login/LoginUI.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/login/LoginUI.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
@@ -21,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,11 +45,12 @@ fun LoginUI() {
             value = email,
             onValueChange = { email = it },
             label = { Text("Email Address") },
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Email, contentDescription = null)
-            },
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = null) },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
         )
+
         Spacer(modifier = Modifier.height(20.dp))
+
         OutlinedTextField(
             value = password,
             onValueChange = { password = it },
@@ -55,9 +58,14 @@ fun LoginUI() {
             leadingIcon = {
                 Icon(imageVector = Icons.Default.Lock, contentDescription = null)
             },
-            visualTransformation = PasswordVisualTransformation()
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done
+            )
         )
+
         Spacer(modifier = Modifier.height(140.dp))
+
         CustomButton(
             buttonText = "Login",
             onClickHandler = { LoginUseCase(context, email, password, response).execute() }
