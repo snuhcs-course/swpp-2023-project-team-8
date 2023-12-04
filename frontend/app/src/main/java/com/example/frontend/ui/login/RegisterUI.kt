eb95522a4ff0a9764606cc6a8b4eafbfd24a0143
heka1024 <heka1024@gmail.com>
2023-12-04 21:37:35 +0900

    Add Next Buttons

--- frontend/app/src/main/java/com/example/frontend/ui/login/RegisterUI.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/login/RegisterUI.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Button
@@ -24,6 +25,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -85,18 +88,26 @@ fun RegisterUI(onSwitchToLogin: () -> Unit) {
             enabled = !isWaitingForResponse.value,
         ) { Text(text = "Send") }
 
+        // Only number and upper case alphabet
         OutlinedTextField(
             value = code,
             onValueChange = { code = it },
             label = { Text("Code") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Characters,
+                imeAction = ImeAction.Next,
+            ),
         )
 
         OutlinedTextField(
             value = name,
             onValueChange = { name = it },
             label = { Text("Name") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next,
+            )
         )
 
         OutlinedTextField(
@@ -104,7 +115,10 @@ fun RegisterUI(onSwitchToLogin: () -> Unit) {
             onValueChange = { password = it },
             label = { Text("Password") },
             visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+            )
         )
 
         Spacer(modifier = Modifier.height(30.dp))
