06148f2d69c5ea812cfb7e974d7a7bc79926adf9
heka1024 <heka1024@gmail.com>
2023-12-06 19:16:15 +0900

    Add KV Store interface

---
+++ frontend/app/src/main/java/com/example/frontend/repository/KVStore.kt
@@ -0,0 +1,12 @@
+package com.example.frontend.repository
+
+/*
+ * Abstract interface for KV store
+ */
+interface KVStore {
+    fun getString(key: String): String?
+    fun putString(key: String, value: String)
+
+    fun getInt(key: String): Int?
+    fun putInt(key: String, value: Int)
+}
