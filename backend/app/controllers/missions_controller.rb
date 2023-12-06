b451f34fef4775aa89efe521de8d5917fdb8aebd
leeeryboy <indiv0227@snu.ac.kr>
2023-12-06 20:53:52 +0900

    GET /missions 응답에 progress 추가

--- backend/app/controllers/missions_controller.rb
+++ backend/app/controllers/missions_controller.rb
@@ -2,7 +2,7 @@ class MissionsController < ApplicationController
 
   def index
     @missions = Mission.left_joins(:user_missions)
-                       .select('missions.*, user_missions.completed as user_completed')
+                       .select('missions.*, user_missions.completed as user_completed, user_missions.progress as user_progress')
                        .where('user_missions.user_id = ? OR user_missions.user_id IS NULL', current_user.id)
 
     render json: @missions, each_serializer: MissionSerializer
