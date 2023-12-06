e2f34f50215a2e2083cc5e0e17ceecf0bcd1ec67
heka1024 <heka1024@gmail.com>
2023-12-06 22:02:41 +0900

    BYPASS_LOGIN false

--- frontend/app/src/main/java/com/example/frontend/utilities/Constants.kt
+++ frontend/app/src/main/java/com/example/frontend/utilities/Constants.kt
@@ -6,4 +6,4 @@ package com.example.frontend.utilities
 
 const val BASE_URL = "http://ec2-13-125-227-45.ap-northeast-2.compute.amazonaws.com"
 
-const val BYPASS_LOGIN = true // 디버깅을 위해 임시로 로그인을 바이패스하는 상수
+const val BYPASS_LOGIN = false // 디버깅을 위해 임시로 로그인을 바이패스하는 상수
