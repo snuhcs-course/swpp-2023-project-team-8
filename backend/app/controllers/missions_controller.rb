6eef179ce0fec8f7c75e6062e0b56dc63071dd27
leeeryboy <indiv0227@snu.ac.kr>
2023-12-06 20:04:15 +0900

    밋업 생성 횟수 트래킹

--- backend/app/controllers/missions_controller.rb
+++ backend/app/controllers/missions_controller.rb
@@ -8,10 +8,4 @@ class MissionsController < ApplicationController
     render json: @missions, each_serializer: MissionSerializer
   end
 
-    def mark_completed
-      mission = Mission.find(params[:id])
-      current_user.toggle_completed(mission)
-      render json: { completed: mission.completed }
-    end
-
 end
