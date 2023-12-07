b9ca703bd317a1ad555d641e064050680af06c65
heka1024 <heka1024@gmail.com>
2023-12-07 14:34:45 +0900

    Serializer

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class UserSerializer < ApplicationSerializer
-  attributes :id, :name, :email, :created_at, :updated_at
+  attributes :id, :name, :email, :created_at, :updated_at, :image_id
 end
