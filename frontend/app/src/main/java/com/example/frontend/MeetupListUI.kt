11c20a3e4674b50e1ab4d2309954084b840445f2
sggithi <seogimin@naver.com>
2023-12-07 21:10:55 +0900

    Log 수정

--- frontend/app/src/main/java/com/example/frontend/MeetupListUI.kt
+++ frontend/app/src/main/java/com/example/frontend/MeetupListUI.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
