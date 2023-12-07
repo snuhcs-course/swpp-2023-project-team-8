a9ebdf7733d193ced674b980a9ab3050da515de8
sggithi <seogimin@naver.com>
2023-12-07 20:31:19 +0900

    Success 확인

--- frontend/app/src/main/java/com/example/frontend/usecase/CreateMeetUpUseCase.kt
+++ frontend/app/src/main/java/com/example/frontend/usecase/CreateMeetUpUseCase.kt
@@ -22,15 +22,16 @@ class CreateMeetUpUseCase(
         call.enqueue(object : Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 if (response.isSuccessful) {
-                    var result = "Create MeetUp Success!!!!: "
+                    var result = "Create MeetUp Success!! "
                     Toast.makeText(context, result, Toast.LENGTH_LONG).show()
                 } else {
+                    Log.d("meetup", response.errorBody().toString())
                     var result = "Create MeetUp failed: " + response.errorBody()?.string()
                     Toast.makeText(context, result, Toast.LENGTH_LONG).show()
                 }
             }
             override fun onFailure(call: Call<Void>, t: Throwable) {
-                Log.d("meetup", "fail!!")
+                Log.d("meetup", "fail!!${t.message}")
                 var result = "Create MeetUp error: " + t.message
                 Toast.makeText(context, result, Toast.LENGTH_LONG).show()
             }
